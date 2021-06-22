574b240aedac799ca71a4e81e0ae7b22a2b0e532
DESKTOP-ELLFJAI\79527 <mr.Tarasyuta@mail.ru>
2021-06-22 15:10:34 +0300

    comments

--- SpaceshipBuildModule/Assets/Scripts/ShipConstruct/Ship.cs
+++ SpaceshipBuildModule/Assets/Scripts/ShipConstruct/Ship.cs
@@ -4,7 +4,7 @@
 
     public class Ship : MonoBehaviour{
         public List<Module> modules { get; private set; }
-        public List<ShipNode> nodes;// { get; private set; }
+        public List<ShipNode> nodes { get; private set; }
 
         private void Awake() {
             modules ??= new List<Module>();
